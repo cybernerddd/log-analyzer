474ab1567e1d0b2799638b1d015cc4e4adfde821
cybernerddd <cybernerddd@gmail.com>
2026-08-06 22:05:52 +0000

    Add common status codes and HTTP methods to config

--- config.py
+++ config.py
@@ -12,3 +12,28 @@ suspicious_agents = [
 "masscan",
 "wpscan"
 ]
+
+# common status codes
+status_codes = [
+"200",
+"201",
+"301",
+"302",
+"400",
+"401",
+"403",
+"404",
+"500",
+"502",
+"503"
+]
+
+# HTTP methods list
+http_methods = [
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE",
+    "OPTIONS",
+    "HEAD"
+]
